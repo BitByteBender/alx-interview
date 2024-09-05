3ec8732896aa1d775021fbb9faf225d1fcb87e34
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-05 07:20:41 +0100

    Check Req

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,10 +5,7 @@ const req = require('request');
 const baseUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 req(baseUrl, (err, res, body) => {
-  if (err) {
-    throw err;
-  }
-
+  if (err) throw err;
   if (res.statusCode !== 200) {
     console.error('status code issue:', res.statusCode);
     return;
@@ -18,14 +15,12 @@ req(baseUrl, (err, res, body) => {
 });
 
 function charsChecker(chars, idx) {
-  if (idx >= chars.length) {
-    return;
-  }
+  if (idx >= chars.length) return;
 
   req(chars[idx], (err, res, body) => {
-    if (err) {
-      throw err;
-    } else if (res.statusCode === 200) {
+    if (err) throw err;
+
+    if (res.statusCode === 200) {
       console.log(JSON.parse(body).name);
     } else {
       console.error('Status code wrong:', res.statusCode);
